2a4d89fd0750e31c83240d6416b12b154669adbb
Vicente <macantivbl@gmail.com>
2021-10-24 07:03:37 -0500

    V8 Logo
    Se agrego el logo de Kalan

--- src/componentes/BarraNabegacion.jsx
+++ src/componentes/BarraNabegacion.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logoInicio from './img/logoInicio.png'
+import logoInicio from './img/KALAN.png'
 
 import {Link} from "react-router-dom";
 const BarraNabegacion = () => {
@@ -20,7 +20,7 @@ const BarraNabegacion = () => {
                         <span className="navbar-toggler-icon">
 
                             <a href="#" className="navbar-brand ">
-                                <img src={logoInicio} alt="" className="mx-auto"
+                                <img  id='logoInicio' src={logoInicio} alt="" className="mx-auto"
 
                                 ></img>
                             </a>
@@ -32,7 +32,11 @@ const BarraNabegacion = () => {
                         <div id='tituloPagina' className="offcanvas-header">
                             <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
                                 <Link to='/'>
-                                    <a className="nav-link" >BBVA</a>
+
+                                    <a className="nav-link" >KALAN</a>
+                                    <img  id='logoInicio2' src={logoInicio} alt="" className="mx-auto"
+
+                                ></img>
                                 </Link></h5>
                             <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
